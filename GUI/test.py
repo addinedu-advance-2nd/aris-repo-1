1a6740125fa55223033d4e12c49db706c0bb6c8b
wooseok0106 <seok970106@gmail.com>
2024-11-07 17:46:45 +0900

    Add files via upload

--- GUI/test.py
+++ GUI/test.py
@@ -10,12 +10,8 @@ import os
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, QStringListModel
-import json
-import socket
-import time
-import threading
-
+from PyQt5.QtCore import Qt, QStringListModel, QTimer
+from PyQt5.QtGui import QPainter, QColor, QPen, QPixmap
 # 파일경로
 base_dir = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(base_dir, "image")
@@ -28,22 +24,23 @@ pixmap3_path = os.path.join(image_dir, "topping1.png")
 pixmap4_path = os.path.join(image_dir, "topping2.png")
 pixmap5_path = os.path.join(image_dir, "topping3.png")
 pixmap6_path = os.path.join(image_dir, "no.png")
-
+position_path = os.path.join(image_dir,"position.png")
 # UI 경로
 main_file_path = os.path.join(base_dir, "untitled.ui")
 topping_file_path = os.path.join(base_dir, "sub.ui")
 memberinfo_file_path = os.path.join(base_dir, "memberinfo.ui")
-
+position_file_path = os.path.join(base_dir,'position.ui')
 main_page_class = uic.loadUiType(main_file_path)[0]
 topping_page_class = uic.loadUiType(topping_file_path)[0]
 memberinfo_page_class = uic.loadUiType(memberinfo_file_path)[0]
-
+position_page_class = uic.loadUiType(position_file_path)[0]
 
 # sub.ui(토핑 선택창) 설정 
 class NewWindow(QDialog, topping_page_class):
     def __init__(self, menu_name, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.setWindowTitle("토핑 선택")
         # 메인 창으로부터 받아오는 값들
         self.image_label = self.findChild(QLabel, 'pixMap_clicked')
         self.text_label = self.findChild(QLabel, 'menu_name_label_clicked')
@@ -126,11 +123,59 @@ class NewWindow(QDialog, topping_page_class):
     def set_image(self, pixmap):
         self.image_label.setPixmap(pixmap)  
         self.image_label.setScaledContents(True)
+
+class PositionLabel(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.circle_x = 50  # 원의 x 좌표
+        self.circle_x_1 = 310  # 원의 x 좌표
+        self.circle_x_2 = 570  # 원의 x 좌표
+
+        self.circle_y = 50  # 원의 y 좌표
+        self.circle_radius = 50  # 원의 반지름
+
+    def paintEvent(self, event):
+        super().paintEvent(event)  # QLabel 기본 이미지 그리기
+
+        # QPainter를 사용해 원 그리기
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        
+        pen = QPen(QColor(255, 0, 0))  # 빨간색 테두리
+        pen.setWidth(5)  # 테두리 두께 설정
+        painter.setPen(pen)
+        painter.setBrush(Qt.NoBrush)  # 내부를 채우지 않음
+        
+        # 원 그리기
+        painter.drawEllipse(self.circle_x, self.circle_y, self.circle_radius * 2, self.circle_radius * 2)
+        painter.drawEllipse(self.circle_x_1, self.circle_y, self.circle_radius * 2, self.circle_radius * 2)
+        painter.drawEllipse(self.circle_x_2, self.circle_y, self.circle_radius * 2, self.circle_radius * 2)
+
+class PositionDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("아이스크림 놓는 위치")
+        
+        # PositionLabel을 사용해 position을 설정
+        self.position = PositionLabel(self)
+        self.position.setGeometry(0, 0, 800, 700)  # 이미지의 위치와 크기 설정
+        self.position.setPixmap(QPixmap(position_path))
+        self.position.setScaledContents(True)
+
+        # 원 좌표 설정
+        self.position.circle_x = 60  # 예제 좌표
+        self.position.circle_y = 450
+        self.position.circle_radius =80
+
+        # 원이 라벨 위에 보이도록 position 라벨 다시 그리기
+        self.position.update()
+
 # 회원정보 
 class MemberInfoDialog(QDialog,memberinfo_page_class):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.setWindowTitle("회원 정보")
         self.mb_cancel_btn.clicked.connect(self.close)
         self.mb_regist_btn.clicked.connect(self.register_member)
     # 회원등록 textedit에 작성한 이름 , 번호를 각각의 name , phone 에 저장
@@ -201,9 +246,20 @@ class WindowClass(QMainWindow, main_page_class):
         self.order_list_widget.setModel(self.order_model) 
         self.receipt.clicked.connect(self.show_receipt) # 임시 영수증 출력
         self.member_btn.clicked.connect(self.open_member_window)
+        # 임시 버튼
+        self.Warning_btn.clicked.connect(self.Warning_event)
+        self.Position_btn.clicked.connect(self.open_position_window)
 
         self.user_name = ""
         self.user_phone = ""
+    # 제조 과정 중 사람 접근 확인 시 안내 문구 - 임시로 버튼 활용
+    def open_position_window(self):
+        position_dialog = PositionDialog(self)
+        position_dialog.exec_()
+
+    def Warning_event(self):
+        QMessageBox.warning(self,'경고','               뒤로 물러나 주세요                  ')
+
 
     def update_member_info(self, name, phone):
         # 받은 이름과 전화번호로 레이블을 업데이트
@@ -219,6 +275,7 @@ class WindowClass(QMainWindow, main_page_class):
         self.new_window.set_image(QPixmap(image_path))
         self.new_window.exec_()
     
+
     def update_order_list(self, order_string):
         current_orders = self.order_model.stringList()
         current_orders.append(order_string)
@@ -226,85 +283,12 @@ class WindowClass(QMainWindow, main_page_class):
 
     def show_receipt(self): #임시 영수증 출력
         current_orders = self.order_model.stringList()
-        # print(my_window.order_model.stringList())
-        order.order_receipt = self.order_model.stringList()
-        order.order_press = True
         receipt_dialog = OrderListDialog(current_orders, self.user_name, self.user_phone, self)
         receipt_dialog.exec_()
-
-
-class OrderClass():
-    def __init__(self):
-        super().__init__()
-        self.order_press = False
-        self.order_receipt = ""
-        
-
-    # def order_receipt_process(self):
-
-
-    
-    def socket_robot(self):
-        self.robot_ADDR = '192.168.0.17'
-        self.robot_PORT = 65432
-        
-        self.robot_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # 추후 시간이 된다면 관리자 페이지를 만들어서 로봇과 통신 연결 / 로봇 테스트 / 설정 변경 등 구현
-        self.robot_client_socket.connect((self.robot_ADDR, self.robot_PORT))
-
-        while True:
-            print("추출 버튼을 눌러주세요")
-            while True:
-                time.sleep(1)
-                if self.order_press == True:
-                    self.order_press = False
-                    break
-            
-            # order_receipt 처리부
-
-
-            msg_type = "icecream"
-            if msg_type == "icecream":
-                jig = 'A' # 사용할 jig  
-                topping_first = False # 아이스크림보다 토핑을 먼저 받을지 말지. True : 먼저 받음
-                topping_no = False # 토핑 받지 않기
-                topping = 'ABC' # A / B / C / AB / AC / BC / ABC
-                topping_time = 2.0 # 총 토핑 받는 시간 == 토핑 량
-                spoon_angle = 180.0 # Angle 기준
-                data_icecream = {
-                    "jig" : jig,
-                    "topping_first" : topping_first,
-                    "topping_no" : topping_no,
-                    "topping" : topping,
-                    "topping_time" : topping_time,
-                    "spoon_angle" : spoon_angle,
-
-                }
-
-                json_data = json.dumps(data_icecream)
-                data = msg_type + '/' + json_data
-            else:
-                data = msg_type
-
-        
-            self.robot_client_socket.sendall(data.encode())
-            
-        
-
-        
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     my_window = WindowClass()
-    order = OrderClass()
-    order_thread = threading.Thread(target=order.socket_robot)
-    order_thread.start()
-    print("order_thread start")
     my_window.show()
-    
-    
-
-
     sys.exit(app.exec_())
